7b7a4369a7db4ad9838950d904dd62426e731ef5
Saro Markosyan <saromark@yandex.ru>
2020-12-22 12:32:46 +0400

    Initial commit

---
+++ periodic_table.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var search = document.getElementById('search');
+
+function init() {
+    var temp = document.getElementById('hydrogen');
+    temp.innerText = 'Hydrogen';
+
+    var temp = document.getElementById('helium');
+    temp.innerText = 'Helium';
+
+    var temp = document.getElementById('lawrencium');
+    temp.innerText = 'Lawrencium';
+}
